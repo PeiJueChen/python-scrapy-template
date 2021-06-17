9a22c4ea3708b32765c46f4c89cf5baf8d9b5adf
风筝 <13286953452@126.com>
2021-06-17 09:13:12 +0800

    file

--- pythonScrapyTemplate/tool/file.py
+++ pythonScrapyTemplate/tool/file.py
@@ -1,21 +1,33 @@
 import os
+
 class File(object):
-    def writeFile(self, content, name = "1.html"):
-        # print('name:',name[0:7])
-        # if name[0:7] != 'static/':
-        #     name = "static/" + name
-        fs = open(name, 'w', encoding="utf-8")
-        fs.write(content)
-        fs.close()
+    def writeFile(self, content, name="1.html"):
+        path = os.path.join(os.path.dirname(
+            os.path.dirname(__file__)), 'file-contents')
+        self.create_dir(path)
+        fullPath = os.path.join(path, name)
+        f = None
+        try:
+            f = open(fullPath, 'w', encoding="utf-8")
+            f.write(content)
+            f.flush()
+        finally:
+            if f:
+                f.close()
+
     def create_dir(self, dir_path):
         # if not os.path.exists(dir_path): os.mkdir(dir_path)
         # 可以创建多层
-        if not self.isExist(dir_path): os.makedirs(dir_path)
+        if not self.isExist(dir_path):
+            os.makedirs(dir_path)
+
     def isExist(self, dir_path):
         return os.path.exists(dir_path)
     # os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
+
     def getCurrentFilePath():
         return os.path.dirname(os.path.dirname(__file__))
+
 
 fileTool = File()
 
